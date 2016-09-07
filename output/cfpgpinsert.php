bbb50b18c277e17cc2d1677f370ef36322c56dde
Jamal Shahverdiev <jamal.shahverdiev@gmail.com>
2016-09-07 22:18:36 +0400

    added empty lines

--- output/cfpgpinsert.php
+++
@@ -1,5 +0,0 @@
-<?php
- $db = pg_connect("host=127.0.0.1 port=5432 dbname=centosdb user=centosuser password=freebsd");
- $query = "INSERT INTO book VALUES ('$_POST[bookid]','$_POST[bookname]', '$_POST[author]','$_POST[publisher]','$_POST[dop]', '$_POST[price]')";
- $result = pg_query($query);
-?>
